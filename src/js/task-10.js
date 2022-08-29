550640f65a12c0995218dcdf70a10e3d76fe74f9
Bolek <103572991+BoloMasta@users.noreply.github.com>
2022-08-29 20:22:57 +0200

    task-10 done

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,25 +1,61 @@
+// FUNKCJA LOSOWANIA KOLORU
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-};
+}
 
-const amount = document.querySelector(".input").value;
+// ZMIENNE
+
+// BOX-SIZE
+var boxSize = "30px";
+
+// INPUT
+var amount = document.querySelector(".input");
+
+// DIV Z KWADRATAMI
 const boxes = document.getElementById("boxes");
-const create = document.querySelector('button[data-create]');
-const destroy = document.querySelector('button[data-destroy]');
 
-console.log(destroy);
+// PRZYCISKI
+const create = document.querySelector("button[data-create]");
+const destroy = document.querySelector("button[data-destroy]");
 
+// FUNKCJA DO ZWIĘKSZANIA KWADRATÓW
+function increaseBox() {
+  let newBoxSize = Number(boxSize.slice(0, boxSize.length - 2));
+  newBoxSize += 10;
+  boxSize = newBoxSize + "px";
+}
+
+// FUNKCJA DO TWORZENIA KWADRATÓW
 function createBoxes(amount) {
-  console.log(amount);
   for (let i = 0; i < amount; i++) {
-    console.log("dsds");
+    const box = document.createElement("div");
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.width = boxSize;
+    box.style.height = boxSize;
+    box.style.float = "left";
+    boxes.append(box);
+
+    increaseBox(boxSize);
   }
-  const box = document.createElement("div");
-  box.style.backgroundColor = "red";
-  boxes.append(box);
-};
+}
 
-create.addEventListener("click", createBoxes(amount));
+// FUNKCJA DO KASOWANIA KWADRATÓW
+function destroyBoxes() {
+  boxes.innerHTML = "";
+  boxSize = "30px";
+}
 
+// NASŁUCHIWANIE
+amount.addEventListener("input", (event) => {
+  amount = event.currentTarget.value;
+});
+
+create.addEventListener("click", () => {
+  createBoxes(amount);
+});
+
+destroy.addEventListener("click", () => {
+  destroyBoxes();
+});
