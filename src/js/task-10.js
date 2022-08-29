cb2be84d41c9b72a5dd40a5b174ab485e32edc00
Bolek <103572991+BoloMasta@users.noreply.github.com>
2022-08-29 16:55:35 +0200

    task-10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,25 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
+
+const amount = document.querySelector(".input").value;
+const boxes = document.getElementById("boxes");
+const create = document.querySelector('button[data-create]');
+const destroy = document.querySelector('button[data-destroy]');
+
+console.log(destroy);
+
+function createBoxes(amount) {
+  console.log(amount);
+  for (let i = 0; i < amount; i++) {
+    console.log("dsds");
+  }
+  const box = document.createElement("div");
+  box.style.backgroundColor = "red";
+  boxes.append(box);
+};
+
+create.addEventListener("click", createBoxes(amount));
+
