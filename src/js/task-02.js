1887cf37790bfdb74b4b5ea7b8a9ca4222c948c7
Bolek <103572991+BoloMasta@users.noreply.github.com>
2022-08-29 12:20:07 +0200

    zrobione 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.getElementById("ingredients");
+
+for (const ingredient of ingredients) {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add("item");
+  list.append(item);
+}
